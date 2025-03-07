8fe5493edb43998e91b2f20fedb31f8bce91c0a4
JulioAlb453 <fulioibeto18@gmail.com>
2025-03-07 10:33:47 -0600

    add event reposository

---
+++ publisher/infraestructure/repository/event_repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"publisher/core"
+	"publisher/publisher/domain"
+)
+
+type EventRepository struct {
+	broker *core.BrokerConnection
+}
+
+func NewEventRepository(broker *core.BrokerConnection) *EventRepository {
+	return &EventRepository{broker: broker}
+}
+
+func (r *EventRepository) Publish(event *domain.Event) error {
+	return r.broker.Publish(event.Payload)
+}
